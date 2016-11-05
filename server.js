b15642bed1da1c153197b7494eda9ade5d792649
Carlos Silva <charlesipn@gmail.com>
2016-11-05 12:53:43 -0600

    ADD body-parser

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
 app.post('/', function (req, res) {
   res.send('POST Hello World!')
